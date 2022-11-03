2ba5cb008a8c0e7ff524ab1f8917d9d264518aab
Nick D'Andrea <nickrdandrea@gmail.com>
2022-11-03 16:29:10 -0400

    Add mutationobserver and move func into callback

--- wtfyoutube.js
+++ wtfyoutube.js
@@ -1,5 +1,23 @@
-const matches = document.querySelectorAll("ytd-shelf-renderer");
-
-for (i = 0; i < matches.length; i++) {
-    matches[i].setAttribute("hidden", "")
+function callback() {
+    let shelves = document.querySelectorAll("ytd-shelf-renderer");
+    if (shelves) {
+        for (i = 0; i < shelves.length; i++) {
+            shelves[i].setAttribute("hidden", "");
+        }
+    }
 }
+
+const targetNodeList = document.querySelectorAll('ytd-page-manager');
+const targetNode = targetNodeList[0]
+
+const observerOptions = {
+    childList: true,
+    subtree: true
+}
+
+const observer = new MutationObserver(callback);
+
+observer.observe(targetNode, observerOptions);
+
+
+
