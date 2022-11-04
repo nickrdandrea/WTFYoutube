5ff98c9b1f21ae9c44a26b11bdd9b554cc94f01a
Nick D'Andrea <nickrdandrea@gmail.com>
2022-11-04 12:48:41 -0400

    Add browser action

--- wtfyoutube.js
+++ wtfyoutube.js
@@ -1,10 +1,19 @@
-function callback() {
-    let shelves = document.querySelectorAll("ytd-shelf-renderer");
-    if (shelves) {
-        for (i = 0; i < shelves.length; i++) {
-            shelves[i].setAttribute("hidden", "");
+// function callback() {
+//     let shelves = document.querySelectorAll("ytd-shelf-renderer");
+//     if (shelves) {
+//         for (i = 0; i < shelves.length; i++) {
+//             shelves[i].setAttribute("hidden", "");
+//         }
+//     }
+// }
+
+function callback(mutationList, observer) {
+    mutationList.forEach((mutation) => {
+        if (mutation.target.nodeName == 'YTD-SHELF-RENDERER') {
+            console.log(mutation.target.querySelector('span').textContent)
+            mutation.target.setAttribute("hidden", "")
         }
-    }
+    });
 }
 
 const targetNodeList = document.querySelectorAll('ytd-page-manager');
